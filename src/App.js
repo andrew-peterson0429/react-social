57f92242be5482ab8cedc00eaa48b70695885643
[andrew.peterson0429] <andrew.peterson1021@gmail.com>
2022-04-20 22:22:46 -0600

    added topbar component and styled, imgs added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import Home from "./pages/home/Home";
+// import DeleteIcon from '@mui/icons-material/Delete';
 
 function App() {
   return (
-    <div >
-      hello
-    </div>
+    <Home/>
   );
 }
 
---
+++ src/pages/home/Home.js
@@ -0,0 +1,9 @@
+// Imports automatically, so dont need
+import React from 'react'
+import Topbar from '../../components/topbar/Topbar';
+
+export default function Home() {
+  return (
+    <Topbar/>
+  )
+}
